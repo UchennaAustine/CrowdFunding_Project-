190032d70a7fa96e650b8c371b52d434d44ab18e
UchennaAustine <uchennaaustine8@gmail.com>
2023-10-06 11:02:28 +0100

    add

---
+++ dist/prisma/migrations/20231006094717_abeg/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "crowdAbeg" (
+    "id" STRING NOT NULL,
+    "userID" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "motivation" STRING NOT NULL,
+    "detailDescription" STRING NOT NULL,
+    "amountNeeded" INT4 NOT NULL,
+    "amountRaised" INT4 NOT NULL,
+    "picture" STRING NOT NULL,
+    "pictureID" STRING NOT NULL,
+    "category" STRING NOT NULL,
+    "givers" JSONB NOT NULL,
+    "love" STRING[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "crowdAbeg_pkey" PRIMARY KEY ("id")
+);
